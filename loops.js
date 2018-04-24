b51ff6908c1f7107ce871b8c1699e51dcb2991fc
Joyce Chun <wooyangee@gmail.com>
2018-04-24 17:56:45 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,18 @@
+function forLoop(array) {
+  var i = 0;
+  for (i; i < 25; i = i+1) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    } else {
+      array.push(`I am ${i} strange loop.`);
+    }
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+  console.log(--n);
+}
+return "done";
+}
