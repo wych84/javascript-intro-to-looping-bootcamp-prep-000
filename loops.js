14d2bb3acd7d118286f016b297bce4e1d3f10522
Joyce Chun <wooyangee@gmail.com>
2018-04-24 18:08:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,8 +21,8 @@ function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5;
   }
-    do {
+  do {
     array.pop();
-      } while (array.length > 0 && maybeTrue())
-      return array;
+  } while (array.length > 0 && maybeTrue())
+  return array;
 }
